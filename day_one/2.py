51ee54a6751230a8c48a24403126f9d5f5b844f7
Selpathor <Selpathor@verizon.net>
2017-11-28 12:44:16 -0500

    Added day 2 exercises

--- day_one/2.py
+++
@@ -1,6 +0,0 @@
-width = float(input("Enter the width of the room"))
-length = float(input("Enter the Length of the room"))
-
-area = width*length
-
-print("THe area of the room is "+ str(area) )
--- day_one/4.py
+++
@@ -1,9 +0,0 @@
-letter = input("Enter a letter: ")
-
-# if letter == "a" or letter == "e" or letter == "i" or letter == "o" or letter == "u":
-if letter in ["a","e","i","o","u"]:
-	print(letter+" is a vowel")
-elif letter == "y":
-	print(letter+" is sometimes a vowel and sometimes a consonant")
-else:
-	print(letter+" is consonant")
